73329f6ab9e087bf6b12bc2daaf4a87761cbaacb
Roel Damalerio <damalerioroel@gmail.com>
2020-08-05 12:06:04 +0800

    Upload project

---
+++ pages/components/Mytext.js
@@ -0,0 +1,22 @@
+// Example: Pre-Populated SQLite Database in React Native
+// https://aboutreact.com/example-of-pre-populated-sqlite-database-in-react-native
+// Custom Text
+
+import React from 'react';
+import { TouchableHighlight, Text, StyleSheet } from 'react-native';
+
+const Mytext = (props) => {
+  return <Text style={styles.text}>{props.text}</Text>;
+};
+
+const styles = StyleSheet.create({
+  text: {
+    color: '#111825',
+    fontSize: 18,
+    marginTop: 16,
+    marginLeft: 35,
+    marginRight: 35,
+  },
+});
+
+export default Mytext;
